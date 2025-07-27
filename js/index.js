265bd2ba6c85bd5b55b3a121487f153b6eaa67c3
wsnrfidev <bisabismillah04@gmail.com>
2025-07-27 19:23:38 +0700

    second commit

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
-const title = document.querySelector('.title')
-const text = `I Have Something For U Gembul`.split('')
+const title = document.querySelector(".title");
+const text = `I Have Something For U Gembul`.split("");
 
 // Create container for better responsive layout
-title.style.display = 'flex'
-title.style.flexWrap = 'wrap'
-title.style.justifyContent = 'center'
-title.style.gap = '0.5rem'
+title.style.display = "flex";
+title.style.flexWrap = "wrap";
+title.style.justifyContent = "center";
+title.style.gap = "0.5rem";
 
 for (let index = 0; index < text.length; index++) {
-  if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}</span>`
+  if (text[index] !== " ") {
+    title.innerHTML += `<span>${text[index]}</span>`;
   } else {
-    title.innerHTML += `<span style='width: 1rem'></span>`
+    title.innerHTML += `<span style='width: 1rem'></span>`;
   }
 }
 
-const textElements = document.querySelectorAll('.title span');
+const textElements = document.querySelectorAll(".title span");
 textElements.forEach((element) => {
   const randomDelay = Math.random() * 3;
   element.style.animationDelay = `${randomDelay}s`;
